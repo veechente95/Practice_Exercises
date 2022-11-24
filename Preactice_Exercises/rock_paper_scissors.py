707d0c6d43a58e13cd815690a396aa2a309610df
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-23 23:19:23 -0800

    Update rock_paper_scissors.py

--- Preactice_Exercises/rock_paper_scissors.py
+++ Preactice_Exercises/rock_paper_scissors.py
@@ -7,4 +7,20 @@ options = ["rock", "paper", "scissors"]
 
 computer = random.choice(options)
 
-selection = input(str("Select rock, paper or scissors: ")).lower()
+user = input(str("Select rock, paper or scissors: ")).lower()
+
+if user == computer:
+    print(f"Computer: {computer}")
+    print("It's a tie")
+elif user == "rock" and computer == "scissors":
+    print(f"Computer: {computer}")
+    print("User Wins")
+elif user == "paper" and computer == "rock":
+    print(f"Computer: {computer}")
+    print("User Wins")
+elif user == "scissors" and computer == "paper":
+    print(f"Computer: {computer}")
+    print("User Wins")
+else:
+    print(f"Computer: {computer}")
+    print("Computer Wins")
