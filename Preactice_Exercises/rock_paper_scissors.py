6ae043870aecbf2fbbe98702030cec15f72dc661
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-23 23:08:55 -0800

    Update rock_paper_scissors.py

--- Preactice_Exercises/rock_paper_scissors.py
+++ Preactice_Exercises/rock_paper_scissors.py
@@ -3,5 +3,8 @@ import random
 rock = "rock"
 paper = "paper"
 scissors = "scissors"
+options = ["rock", "paper", "scissors"]
 
-pri
+computer = random.choice(options)
+
+selection = input(str("Select rock, paper or scissors: ")).lower()
