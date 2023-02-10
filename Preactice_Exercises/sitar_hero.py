9a885c2ad77ccdbdcd447480736b66de283d09ea
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-02-09 22:26:10 -0800

    Update sitar_hero.py

--- Preactice_Exercises/sitar_hero.py
+++ Preactice_Exercises/sitar_hero.py
@@ -6,3 +6,8 @@
 normal_note = ["g", "r", "y", "b", "o"]
 sustained_note = ["G", "R", "Y", "B", "O"]
 no_note = "x"
+
+# (Notice that the instructions gggb and GGGb are different. 
+# The first means "press and release the green button for each of the first three beats, then press and release the blue button", 
+# while the second means "press the green button on the first beat, hold it there until you release it at the end of the third beat, 
+# then press and release the blue button".)
