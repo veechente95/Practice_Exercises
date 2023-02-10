2b4aad5572778fc8839a73ad565fcae9fc132dcc
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-02-09 21:45:50 -0800

    Update water_works.py

--- Preactice_Exercises/water_works.py
+++ Preactice_Exercises/water_works.py
@@ -23,7 +23,7 @@ def tier_rate():
             return low_second_rate
         return first_rate
 
-
+# While loop iterattion through test cases.
 should_continue = False
 while not should_continue:
     initial_reading = int(input("Initial meter reading: "))
@@ -32,27 +32,64 @@ while not should_continue:
     else:
         should_continue = True
 
-    while should_continue:
-        final_reading = int((input("Final meter reading: ")))
-        if initial_reading > final_reading:
-            print("The final meter reading must be at least as large as the initial reading.")
-        else:
-            should_continue = False
+should_continue = False
+while not should_continue:
+    final_reading = int((input("Final meter reading: ")))
+    if final_reading < initial_reading:
+        print("The final meter reading must be at least as large as the initial reading.")
+    else:
+        should_continue = True
 
-        while not should_continue:
-            customer_name = str(input("Customer name: "))
-            if customer_name == "":
-                print("You must enter a customer name.")
-            else:
-                should_continue = True
+should_continue = False
+while not should_continue:
+    customer_name = str(input("Customer name: "))
+    if customer_name == "":
+        print("You must enter a customer name.")
+    else:
+        should_continue = True
 
-            while should_continue:
-                month_num = int(input("Month number (1=Jan, 2=Feb, etc.): "))
-                if month_num != month_num >= 1 or month_num != month_num <= 12:
-                    print("The month number must be in the range 1 through 12.")
-                else:
-                    should_continue = False
+should_continue = False
+while not should_continue:
+    month_num = int(input("Month number (1=Jan, 2=Feb, etc.): "))
+    if month_num <= 0 or month_num > 12:
+        print("The month number must be in the range 1 through 12.")
+    else:
+        should_continue = True
 
-            print("\n---")
-            print(f"The bill for {customer_name} is ${round(tier_rate(), 2)}")
+print("\n---")
+print(f"The bill for {customer_name} is ${float(round(tier_rate(), 3))}")
+
+
+# ----PREVIOUS ATTEMPT -----
+# should_continue = False
+# while not should_continue:
+#     initial_reading = int(input("Initial meter reading: "))
+#     if initial_reading < 0:
+#         print("The initial meter reading must not be negative.")
+#     else:
+#         should_continue = True
+
+#     while should_continue:
+#         final_reading = int((input("Final meter reading: ")))
+#         if initial_reading > final_reading:
+#             print("The final meter reading must be at least as large as the initial reading.")
+#         else:
+#             should_continue = False
+
+#         while not should_continue:
+#             customer_name = str(input("Customer name: "))
+#             if customer_name == "":
+#                 print("You must enter a customer name.")
+#             else:
+#                 should_continue = True
+
+#             while should_continue:
+#                 month_num = int(input("Month number (1=Jan, 2=Feb, etc.): "))
+#                 if month_num != month_num >= 1 or month_num != month_num <= 12:
+#                     print("The month number must be in the range 1 through 12.")
+#                 else:
+#                     should_continue = False
+
+#             print("\n---")
+#             print(f"The bill for {customer_name} is ${round(tier_rate(), 2)}")
 
