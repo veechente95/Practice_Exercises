f6030139c906dd1fdd245ace2453a43cbacb7dd3
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-02-09 22:24:57 -0800

    Create sitar_hero.py

---
+++ Preactice_Exercises/sitar_hero.py
@@ -0,0 +1,8 @@
+# https://web.archive.org/web/20220429062001/http://web.cs.ucla.edu/classes/spring22/cs31/
+
+# normal note = press and release note
+# sustained note = press and hold note
+
+normal_note = ["g", "r", "y", "b", "o"]
+sustained_note = ["G", "R", "Y", "B", "O"]
+no_note = "x"
