23e215dc39bc0158ecf40768840b566ef98f5b0b
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-02-15 21:14:54 -0800

    Update water_works.py

--- Preactice_Exercises/water_works.py
+++ Preactice_Exercises/water_works.py
@@ -4,7 +4,7 @@ second_tier_rate_low = 7.77
 second_tier_rate_high = 9.79
 
 
-def tier_rate(month_num):
+def tier_rate(month_num, initial_reading, final_reading):
     """Returns HCF rates based on low and high usage seasons."""
     hcf = final_reading - initial_reading
     first_rate = hcf * first_tier_rate
@@ -15,33 +15,34 @@ def tier_rate(month_num):
         if hcf > 23:
             hcf_high_difference = hcf - 23
             high_second_rate = (hcf_high_difference * second_tier_rate_high) + (23 * first_tier_rate)
-            return high_second_rate
-        return first_rate
+            return round(high_second_rate, 3)
+        return round(first_rate, 3)
 
     # --- Low Usage Season ---
     if month_num in (1, 2, 3, 11, 12):
         if hcf > 15:
             hcf_low_difference = hcf - 15
             low_second_rate = (hcf_low_difference * second_tier_rate_low) + (15 * first_tier_rate)
-            return low_second_rate
-        return first_rate
+            return round(low_second_rate, 3)
+        return round(first_rate, 3)
 
 
-initial_reading = int(input("Initial meter reading: "))
-if initial_reading < 0:
-    print("The initial meter reading must not be negative.")
-final_reading = int((input("Final meter reading: ")))
-if final_reading < initial_reading:
-    print("The final meter reading must be at least as large as the initial reading.")
-customer_name = str(input("Customer name: "))
-if customer_name == "":
-    print("You must enter a customer name.")
-month_num = int(input("Month number (1=Jan, 2=Feb, etc.): "))
-if month_num <= 0 or month_num > 12:
-    print("The month number must be in the range 1 through 12.")
+if __name__ == "__main__":
+    initial_reading = int(input("Initial meter reading: "))
+    if initial_reading < 0:
+        print("The initial meter reading must not be negative.")
+    final_reading = int((input("Final meter reading: ")))
+    if final_reading < initial_reading:
+        print("The final meter reading must be at least as large as the initial reading.")
+    customer_name = str(input("Customer name: "))
+    if customer_name == "":
+        print("You must enter a customer name.")
+    month_num = int(input("Month number (1=Jan, 2=Feb, etc.): "))
+    if month_num <= 0 or month_num > 12:
+        print("The month number must be in the range 1 through 12.")
 
-print("\n---")
-print(f"The bill for {customer_name} is ${float(round(tier_rate(), 3))}")
+    print("\n---")
+    print(f"The bill for {customer_name} is ${float(round(tier_rate(month_num, initial_reading, final_reading), 3))}")
 
 
 # ----- Program improvement using loops to iterate until valid input are made -----
