b42d4b3a93f0a542c9c567822c322484ad8b3f56
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-03-14 18:03:00 -0700

    Update sitar_hero.py

--- Preactice_Exercises/sitar_hero.py
+++ Preactice_Exercises/sitar_hero.py
@@ -1,34 +1,43 @@
 # https://web.archive.org/web/20220429062001/http://web.cs.ucla.edu/classes/spring22/cs31/
 import random
 
-normal_note = "g", "r", "y", "b", "o"
-sustained_note = "G", "R", "Y", "B", "O"
-no_note = "x"
+# normal_note = "g", "r", "y", "b", "o"
+# sustained_note = "G", "R", "Y", "B", "O"
+# no_note = "x"
 colors = "G", "g", "R", "r", "Y", "y", "B", "b", "O", "o"
-digit = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-end_beat = "/", ""
+digit = "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+beat = {
+    "slash": "/",
+    "color_slash": random.choice(colors) + "/",
+    "color_digit_slash": random.choice(colors) + random.choice(digit) + "/",
+    "color_2digits_slash": random.choice(colors) + random.choice(digit) + random.choice(digit) + "/",
+}
+
+print(list(beat.values()))
+
+
+def has_proper_syntax(tune):   # Checks if syntactically correct.
+    # Ends with "/"
+    if tune[-1] == "/":
+        return True
+    else:
+        return False
+
+
 
 
 # color and digit
 color_dict = {
-    "G": 0,
-    "g": 2,
-    "R": 3,
-    "r": 4,
-    "Y": 5,
-    "y": 6,
-    "B": 7,
-    "b": 8,
-    "O": 9,
-    "o": 1,
+    "G": "00",
+    "g": "01",
+    "R": "02",
+    "r": "03",
+    "Y": "04",
+    "y": "05",
+    "B": "06",
+    "b": "07",
+    "O": "08",
+    "o": "09",
 }
 
-note_progression = random.choice(colors) + str(random.choice(digit)) + random.choice(end_beat)
-print(note_progression)
-
-
-# def syntactically_correct():
-for beat in note_progression:
-    if beat[-1] == "/":
-        if len("/" > 0):
-            return True
+# def convert_tune(tune, instructions, bad_beat):
